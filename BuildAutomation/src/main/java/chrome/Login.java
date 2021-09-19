7d4d2752b10ee13d07f06aa567160eb5c5a33401
AlexanderKanickaisamy <antonyalexander65@gmail.com>
2021-09-19 17:39:56 +0530

    Update Login.java

--- BuildAutomation/src/main/java/chrome/Login.java
+++ BuildAutomation/src/main/java/chrome/Login.java
@@ -33,7 +33,7 @@ public class Login {
 			driver = new FirefoxDriver();
 		}
 
-
+driver.get("http://leaftaps.com/opentaps");
 		driver.get("http://leaftaps.com/opentaps");
 		driver.findElementById("username").sendKeys("DemoSalesManager");
 		driver.findElementById("password").sendKeys("crmsfa");
